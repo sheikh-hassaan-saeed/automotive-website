b7b05858875f233eb9a531ead3d127be50f1b7e4
Sheikh Hassaan Saeed <sheikhhassaansaeed@gmail.com>
2025-10-30 22:37:19 +0500

    Add files via upload

---
+++ AutoCare/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#DC2626',
+        dark: '#1a1a1a',
+      },
+    },
+  },
+  plugins: [],
+}
